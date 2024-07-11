8f1ae823c91cb7efd7b62afa6a41ff0d316428fe
iqbalalfarizy <iqbalalfarizy06@gmail.com>
2024-07-11 17:16:26 +0700

    penambahan mocha-tags

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -5,21 +5,14 @@ class LoginWeb{
     get isiEmail () { return $('input[name="username"]') }
     get isiPassw () { return $('input[name="password"]') }
     get loginBtn () { return $('button.btn.mt-6.text-white.shadow-md.btn-primary.btn-block') }
-    get TAbtn()     { return $('a[href="/applicant/dashboard"]')}
     
 
     //Page Actions
     async loginProccess(namaUser, passUser){
+        await browser.url('https://app.prosigmaka.com/login')
         await this.isiEmail.setValue(namaUser)
         await this.isiPassw.setValue(passUser)
         await this.loginBtn.click()
-    }
-    async namaWeb(){
-        await browser.url('https://app.prosigmaka.com/login')
-    }
-
-    async menuTA(){
-        await this.TAbtn.click()
     }
 }
 
