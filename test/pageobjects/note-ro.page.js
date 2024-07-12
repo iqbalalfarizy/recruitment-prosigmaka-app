32dbb3af0eecbc72134c9ca37390d6c1a9765508
Syaadillah <dillahramdhan@gmail.com>
2024-07-12 15:31:11 +0700

    Menu Note RO

--- test/pageobjects/note-ro.page.js
+++ test/pageobjects/note-ro.page.js
@@ -2,7 +2,146 @@ import { browser, $, expect } from '@wdio/globals'
 
 
 class HalamanNoteRO {
+    //Element Locator
+    get ALbtn           (){ return $('a[href="/recruitment/applicant/list"]')}
+    get Hamburgerbtn    (){ return $('//*[@id="root"]/div[1]/div/main/div/div/div/div/table/tbody/tr[2]/td[9]/section/button')}
+    get updtresumebtn   (){ return $('a[href="/recruitment/applicant/resume/15"]')}
+    get detailbtn       (){ return $('a[href="/recruitment/applicant/detail/15"]')}
+    get noteRObtn       (){ return $('//*[@id="root"]/div[1]/div/main/div/div/div/div/div/button[8]')}
+    get nameUpdate      (){ return $('//td[span[text()="Sugeng Sugiono"]]/span')}
+    get nameNoteRO      (){ return $('h2.card-title')}
     
+    get ketALblcklist   (){ return $('//td[contains(span, "Blacklist")]')}
+    get ketALnotavail   (){ return $('//td[contains(span, "Not Available")]')}
+    get ketALavail      (){ return $('//td[contains(span, "Available")]')}
+    get ketALfail       (){ return $('//td[contains(span, "RO FAIL")]')}
+    get ketALpass       (){ return $('//td[contains(span, "RO PASS")]')}
+    get ketALneedCV     (){ return $('//td[contains(span, "CV Need Update")]')}
+    
+    //aplicant list
+    get blcklistbtn     (){ return $('//button[contains(text(), "Blacklist")]')}
+    get ketBLmenuRO     (){ return $('//div[contains(text(), "Blacklist")]')}
+    get notavailbtn     (){ return $('//button[contains(text(), "Not Available")]')}
+    get ketNAmenuRO     (){ return $('//div[contains(text(), "Not Available")]')}
+    get setavailbtn     (){ return $('//button[contains(text(), "Set Available")]')}
+    get failbtn         (){ return $('//button[contains(text(), "Fail")]')}
+    get passbtn         (){ return $('//button[contains(text(), "Pass")]')}
+    get needCVupdate    (){ return $('//button[contains(text(), "Need to Update CV")]')}
+    get popupModal      (){ return $$('//div[@role="status" and @aria-live="polite"]')}
 
+    //form Note RO
+    get UbahDatabtn     (){ return $('//button[contains(text(), "Ubah Data")]')}
+    get formDom         (){ return $('input[name="domisili"]')}
+    get SubmitDatabtn   (){ return $('//button[contains(text(), "Submit")]')}
+    get CancelDatabtn   (){ return $('//button[contains(text(), "Batal")]')}
+    get formProjectBased(){ return $('select[name="lamaran.onsite"]')}
+    get popupSukses     (){ return $('div[role="status"][aria-live="polite"]')}
+    get katkandidat     (){ return $('#react-select-6-live-region ~ .react-select__control')}
+    get katBootcamp     (){ return $('#react-select-6-option-0')}
+    get kesiapanJoin    (){ return $('#react-select-7-live-region ~ .react-select__indicators')}
+    get siapASAP        (){ return $('#react-select-7-option-0')}
+    get statusRekom     (){ return $('#react-select-8-live-region ~ .react-select__indicators')}
+    get rekom           (){ return $('#react-select-8-option-0')}
+    get gradeSalary     (){ return $('#react-select-9-live-region ~ .react-select__indicators')}
+    get Salary1         (){ return $('#react-select-9-option-0')}
 
+    //detail
+    get nameDetail      (){ return $('//td[span[text()="Sugeng Sugiono"]]/span')}
+    get nameMenuDet     (){ return $('h2.card-title')}
+    get detReadyOns     (){ return $('//p[contains(text(), "Ready onsite")]/following-sibling::p')}
+    
+    
+    //Page Actions
+    async ALpage(){
+        await this.ALbtn.click()
+    }
+    async UpRespage(){
+        await this.Hamburgerbtn.click()
+        await this.updtresumebtn.click()
+    }
+    async DetPage(){
+        await this.Hamburgerbtn.click()
+        await this.detailbtn.click()
+    }
+    async TabUpdateResume(){
+        const handles = await browser.getWindowHandles()
+        await browser.switchToWindow(handles[1])
+    }
+    async TabDetail(){
+        const handles = await browser.getWindowHandles()
+        await browser.switchToWindow(handles[2])
+    }
+    async TabAplicantList(){
+        const handles = await browser.getWindowHandles()
+        await browser.switchToWindow(handles[0])
+    }
+    async MenuNoteRO(){
+        await this.noteRObtn.click()
+    }
+    async getNameUpdate(){
+        const namaygdiupdate= this.nameUpdate.getText()
+        return namaygdiupdate
+    }
+    async getNameDetail(){
+        const namaygdidetail= this.nameUpdate.getText()
+        return namaygdidetail
+    }
+    async blacklistRO(){
+        await this.blcklistbtn.click()
+    }
+    async notavailRO(){
+        await this.notavailbtn.click()
+    }
+    async setavailRO(){
+        await this.setavailbtn.click()
+    }
+    async failRO(){
+        await this.failbtn.click()
+    }
+    async passRO(){
+        await this.passbtn.click()
+    }
+    async KetBLmenuRO(){
+        const keterangan = await this.ketBLmenuRO.getText()
+        return keterangan
+    }
+    async KetNAmenuRO(){
+        const keterangan = await this.ketNAmenuRO.getText()
+        return keterangan
+    }
+    async updateCVRO(){
+        await this.needCVupdate.click()
+    }
+    async UbahData(){
+        await this.UbahDatabtn.click()
+    }
+    async SubmitData(){
+        await this.SubmitDatabtn.click()
+    }
+    async CancelData(){
+        await this.CancelDatabtn.click()
+    }
+    async setDom(dom){
+        await this.formDom.setValue(dom)
+    }
+    async isiDom(){
+        const isiDom = await this.formDom.getText()
+        return isiDom
+    }
+    async setProjectBasedYa(){
+        await this.formProjectBased.scrollIntoView()
+        await this.formProjectBased.selectByAttribute('value', 'true');
+    }
+    async setProjectBasedNo(){
+        await this.formProjectBased.scrollIntoView()
+        await this.formProjectBased.selectByAttribute('value', 'false');
+    }
+    async formKatKandidat(){
+        await this.katkandidat.click()
+    }
+    async formKatBootcamp(){
+        await this.katBootcamp.click()
+    }
 }
+
+export default new HalamanNoteRO()
--- test/specs/note-ro.spec.js
+++ test/specs/note-ro.spec.js
@@ -1,16 +1,236 @@
 import { browser, $, expect } from '@wdio/globals'
 import authLogin from '../pageobjects/page.js'
-
+import NoteRO from '../pageobjects/note-ro.page.js'
 
 describe('UJI Halaman Note RO Recruitment App Pro Sigmaka', function(){
     before('User Login Dulu', async function(){
         await authLogin.namaWeb()
         await authLogin.loginProccess('dummy@prosigmaka.com','dummypsm')
         await authLogin.menuTA()
+        await NoteRO.ALpage()
     })
-    describe('Uji Button Note RO', function(){
-        it('Test', async function(){
-            await browser.pause(10000)
+
+    describe('(1). Uji Menu Note RO', function(){
+        it('Sudah Berada di Menu Note RO', async function(){
+            await NoteRO.UpRespage()
+            const nama = await NoteRO.getNameUpdate()
+            await NoteRO.TabUpdateResume()
+            await NoteRO.MenuNoteRO()
+            await expect(NoteRO.nameNoteRO).toHaveText(
+                expect.stringContaining(nama)
+            )
+            await browser.pause(2000)
+            
+        })
+    })
+    describe('(MAL.01.001). Uji Button Note RO', function(){
+        it('Klik Blacklist Button ', async function(){
+            //Klik Button Blacklist
+            await NoteRO.TabUpdateResume()
+            await NoteRO.blacklistRO()
+            const keterangan = await NoteRO.KetBLmenuRO()
+            await browser.pause(2000)
+            //Pindah ke Tab Applicant List
+            await NoteRO.TabAplicantList()
+            await browser.refresh()
+            //Assertion
+            await expect(NoteRO.ketALblcklist).toHaveText(
+                expect.stringContaining(keterangan)
+            )
+            await browser.pause(2000)
+        })
+    })
+
+    describe('(MAL.01.002). Uji Button Note RO', function(){
+        it('Un-blacklist Button dan Set menjadi Not Available ', async function(){
+            //Kembalikan button ke semula
+            await NoteRO.TabUpdateResume()
+            await NoteRO.blacklistRO()
+            await browser.pause(2000)
+            //Klik Button Not Avail
+            await NoteRO.notavailRO()
+            await browser.pause(2000)
+            const keterangan = await NoteRO.KetNAmenuRO()
+            //Pindah ke Tab Applicant List
+            await NoteRO.TabAplicantList()
+            await browser.refresh()
+            //Assertion
+            await expect(NoteRO.ketALnotavail).toHaveText(
+                expect.stringContaining(keterangan)
+            )
+            await browser.pause(2000)
+        })
+    })
+    
+    describe('(MAL.01.003). Uji Button Note RO', function(){
+        it('Ubah Button Not Available menjadi Set Available ', async function(){
+            //Klik button Set Available
+            await NoteRO.TabUpdateResume()
+            await NoteRO.setavailRO()
+            await browser.pause(2000)
+            //Pindah ke Tab Applicant List
+            await NoteRO.TabAplicantList()
+            await browser.refresh()
+            //Assertion
+            await expect(NoteRO.ketALavail).toHaveText('Available')
+            await browser.pause(2000)
+        })
+    })
+
+    describe('(MAL.01.004). Uji Button Note RO', function(){
+        it('Klik Button Fail', async function(){
+            //Klik button Fail
+            await NoteRO.TabUpdateResume()
+            await NoteRO.failRO()
+            await browser.pause(2000)
+            //Pindah ke Tab Applicant List
+            await NoteRO.TabAplicantList()
+            await browser.refresh()
+            //Assertion
+            await expect(NoteRO.ketALfail).toHaveText('RO FAIL')
+            await browser.pause(2000)
+        })
+    })
+
+    describe('(MAL.01.005). Uji Button Note RO', function(){
+        it('Klik Button Pass', async function(){
+            //Klik button Fail
+            await NoteRO.TabUpdateResume()
+            await NoteRO.passRO()
+            await browser.pause(2000)
+            //Pindah ke Tab Applicant List
+            await NoteRO.TabAplicantList()
+            await browser.refresh()
+            //Assertion
+            await expect(NoteRO.ketALpass).toHaveText('RO PASS')
+            await browser.pause(2000)
+        })
+    })
+
+    describe('(MAL.01.006). Uji Button Note RO', function(){
+        it('Klik Button Need Update CV', async function(){
+            //Klik button biru
+            await NoteRO.TabUpdateResume()
+            await NoteRO.updateCVRO()
+            await browser.pause(2000)
+            //Pindah ke Tab Applicant List
+            await NoteRO.TabAplicantList()
+            await browser.refresh()
+            //Assertion
+            await expect(NoteRO.ketALneedCV).toHaveText('CV Need Update')
+            await browser.pause(2000)
+        })
+    })
+
+    describe('Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan form baru bisa di manipulasi', async function(){
+            //Klik button biru
+            await NoteRO.TabUpdateResume()
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+
+            //Assertion
+            const formReadOnly = await NoteRO.formDom.getAttribute('readonly')
+            await expect(formReadOnly).toBe(null)
+            await NoteRO.CancelData()
+        })
+    })
+    describe('Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan klik tombol submit', async function(){
+            //Klik button biru
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+
+            //Assertion
+            await NoteRO.SubmitData()
+            const formReadOnly = await NoteRO.formDom.getAttribute('readonly')
+            await expect(formReadOnly).toBe('true')
+        })
+    })
+    
+    describe('(MDP.01.001). Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan isi form Project Based', async function(){
+            //Klik button biru
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+
+            //Assertion
+            await NoteRO.setProjectBasedYa()
+            await NoteRO.SubmitData()
+            await browser.pause(2000)
+            //////
+            await NoteRO.TabAplicantList()
+            await NoteRO.DetPage()
+            await NoteRO.TabDetail()
+            await browser.pause(2000)
+            await expect(NoteRO.detReadyOns).toHaveText('Ya')
+        })
+    })
+    describe('(MDP.01.002). Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan isi form Project Based', async function(){
+            //Klik button biru
+            await NoteRO.TabUpdateResume()
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+
+            //Assertion
+            await NoteRO.setProjectBasedNo()
+            await NoteRO.SubmitData()
+            await browser.pause(2000)
+            //////
+            await NoteRO.TabDetail()
+            await browser.refresh() 
+            await browser.pause(2000)
+            await expect(NoteRO.detReadyOns).toHaveText('Tidak')
+        })
+    })
+
+    describe('(MDP.01.003). Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan Isi form Domisili', async function(){
+            //Klik button biru
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+            const setDom = await NoteRO.setDom('Jakarta')
+            await NoteRO.SubmitData()
+            
+            //Assertion
+            const isiDom = await NoteRO.isiDom()
+            await expect(setDom).toHaveText(
+                expect.stringContaining(isiDom)
+            )
+            await browser.pause(2000)
+        })
+    })
+    describe('(MDP.01.004). Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan Isi form Kategori', async function(){
+            //Klik button biru
+            await NoteRO.TabUpdateResume()
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+            await NoteRO.formKatKandidat()
+            await browser.pause(2000)
+            await NoteRO.formKatBootcamp()
+            await NoteRO.SubmitData()
+            await browser.pause(2000)
+            //Assertion
+            const muncul = await NoteRO.popupSukses.isDisplayed();
+            await expect(muncul).toBe(true)
+            await browser.pause(2000)
+        })
+    })
+    describe('(MDP.01.005). Uji Manipulasi Form Note RO', function(){
+        it('Klik Button Ubah Data, dan Isi form Kesiapan Join', async function(){
+            //Klik button biru
+            await NoteRO.TabUpdateResume()
+            await NoteRO.UbahData()
+            await browser.pause(2000)
+            await NoteRO.formKatKandidat()
+            await browser.pause(2000)
+            await NoteRO.formKatBootcamp()
+            await browser.pause(2000)
+            //Assertion
+            
+            await browser.pause(2000)
         })
     })
 })
