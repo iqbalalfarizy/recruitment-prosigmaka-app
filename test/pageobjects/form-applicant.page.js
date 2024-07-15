633862ed960e1eda3d96a2b62dbd0bf685b611e5
mahadipaagung <73451802+mahadipaagung@users.noreply.github.com>
2024-07-15 16:59:42 +0800

    Update form-applicants

--- test/pageobjects/form-applicant.page.js
+++ test/pageobjects/form-applicant.page.js
@@ -5,12 +5,24 @@ class JobApplicantForm {
     get namaInput() { return $('input[name="nama"]')}
     get emailInput() { return $('input[type="email"]')}
     get nohp() { return $('input[type="tel"]')}
-    get edubackground() { return $('.react-select__dropdown-indicator')}
-    get edubackgroundmenu() { return $('.react-select__menu')}
+    get edubackground() { return $('#react-select-2-live-region ~ .react-select__control')}
+    get edubackgroundmenu() { return $('.react-select__option=S1')}
     get addrolebtn() { return $('.btn.text-white.btn-primary')}
     get rolecardtambahan() { return $('.card.bg-base-100.shadow.flex-1.w-full.max-w-2xl:nth-child(3)')}
+    get rolechoose() { return $('#react-select-3-live-region ~ .react-select__control')}
+    get rolechoosemenu() { return $('.react-select__option=Android Native Developer')}
+    get rolechoosemenu2() { return $('.react-select__option=Angular Developer')}
+    get yearexp() { return $('#react-select-4-live-region ~ .react-select__control')}
+    get chooseyearexp() { return $('.react-select__option=2 tahun')}
+    get skills() { return $('#react-select-5-live-region ~ .react-select__control')}
+    get chooseskillsjira() { return $('.react-select__option=Jira')}
+    get chooseskillsnodejs() { return $('.react-select__option=Node JS')}
+    get optionalskills() { return $('div.form-control > label.input > input[name=toolsFreeText]')}
     get onsite() { return $('select[name="lamaran.onsite"]')}
     get joinreadiness() { return $('select[name="lamaran.kesiapanJoin"]')}
+    get currentsalary() { return $('input[name="lamaran.currentSalary"]')}
+    get expectedsalary() { return $('input[name="lamaran.expectedSalary"]')}
+    get submitbtn() { return $('button.btn.mt-4.text-white.btn-primary')}
 
     //Functions
     async sideNavJAFormClick() {
@@ -25,6 +37,10 @@ class JobApplicantForm {
     async selectjoinreadiness(str) {
         await this.joinreadiness.click()
         await this.joinreadiness.selectByAttribute('value', str)
+    }
+
+    async submitbtnClick(){
+        await this.submitbtn.click()
     }
 }
 
--- test/specs/form-applicants.spec.js
+++ test/specs/form-applicants.spec.js
@@ -2,13 +2,11 @@ import {browser, expect} from '@wdio/globals'
 import LoginWeb from '../pageobjects/page.js'
 import JobApplicationForm from '../pageobjects/form-applicant.page.js'
 
-describe('Testing Job Applicant Input Form', () => {
+describe('(JAF.001.01)Testing Job Applicant Input Form', () => {
     before(async () => {
-        await LoginWeb.namaWeb()
         await LoginWeb.loginProccess('dummy@prosigmaka.com', 'dummypsm')
-        await LoginWeb.menuTA()
+        await LoginWeb.TAbtn.click()
         await JobApplicationForm.sideNavJAFormClick()
-
     })
     it('Checking input form Nama lengkap with positive value, and check required attributes', async () => {
         await JobApplicationForm.namaInput.setValue('Sharon Callie')
@@ -25,13 +23,31 @@ describe('Testing Job Applicant Input Form', () => {
     it('Selecting educational background dropdown list with positive value', async () => {
         await JobApplicationForm.edubackground.click()
         await JobApplicationForm.edubackgroundmenu.waitForDisplayed()
-        await JobApplicationForm.edubackgroundmenu.selectByAttribute('input[aria-activedescendant="react-select-2-option-2"]')
-        // await browser.pause(3000)
+        await JobApplicationForm.edubackgroundmenu.click()
         expect(JobApplicationForm.edubackground).toHaveValue('S1')
+    })
+    it('Input birthdate form', async () => {
+        await browser.execute(() => {
+            document.querySelector('[name=tanggalLahir]').value = '2001-07-05'
+        })
     })
     it('Adding previous role by the button', async () => {
         await JobApplicationForm.addrolebtn.click()
         expect(JobApplicationForm.rolecardtambahan).toBeDisplayed()
+    })
+    it('Choosing past role experience', async () => {
+        await JobApplicationForm.rolechoose.click()
+        await JobApplicationForm.rolechoosemenu.click()
+    })
+    it('Choosing past working time', async () => {
+        await JobApplicationForm.yearexp.click()
+        await JobApplicationForm.chooseyearexp.click()
+    })
+    it('Choosing professional skills', async () => {
+        await JobApplicationForm.skills.click()
+        await JobApplicationForm.chooseskillsjira.click()
+        await JobApplicationForm.skills.click()
+        await JobApplicationForm.chooseskillsnodejs.click()
     })
     it('Checking on-site readiness form with positive value and having the required attributes', async () => {
         await JobApplicationForm.selectOnsite("ya")
@@ -40,8 +56,35 @@ describe('Testing Job Applicant Input Form', () => {
     })
     it('Checking Join Readiness form with positive value and having the required attributes', async () => {
         await JobApplicationForm.selectjoinreadiness('ASAP')
-        await browser.pause(3000)
         expect(JobApplicationForm.joinreadiness).toHaveValue('ASAP')
         expect(JobApplicationForm.joinreadiness).toHaveAttribute('required')
+    })
+    it('Filling current salary with positive data value', async () => {
+        await JobApplicationForm.currentsalary.setValue(8000000)
+    })
+    it('Filling expected salary with positive data value', async () => {
+        await JobApplicationForm.expectedsalary.setValue(10000000)
+        await browser.pause(3000)
+    })
+})
+describe('Testing Job Application Form with negative and void value', () => {
+    before(async () => {
+        await browser.url('https://app.prosigmaka.com')
+        await LoginWeb.TAbtn.click()
+        await JobApplicationForm.sideNavJAFormClick()
+    })
+    it('(JAF.001.02) Submitting form without filling value on Nama Lengkap', async () => {
+        await JobApplicationForm.submitbtnClick()
+        expect(JobApplicationForm.submitbtn).toHaveAttribute('required')
+    })
+    it('(JAF.001.03) Submitting form without filling value on Email', async () => {
+        await JobApplicationForm.namaInput.setValue('Sharon Callista')
+        await JobApplicationForm.submitbtnClick()
+    })
+    it('(JAF.001.04) Submitting form without filling value on Phone Number', async () => {
+        await JobApplicationForm.emailInput.setValue('sharon')
+        await browser.pause(5000)
+        expect(JobApplicationForm.emailInput).toHaveAttribute('input[type=email]')
+        expect(JobApplicationForm.emailInput).toHaveAttribute('required')
     })
 })
